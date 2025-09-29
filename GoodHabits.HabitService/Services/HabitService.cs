59b08e5c694cd948873a3800378956b1501f3b61
el2toro <vasea.badragan@gmail.com>
2025-09-29 10:53:04 +0000

    Add and implement habit service

---
+++ GoodHabits.HabitService/Services/HabitService.cs
@@ -0,0 +1,31 @@
+using GoodHabits.Database;
+using GoodHabits.Database.Entities;
+using GoodHabits.HabitService.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodHabits.HabitService.Services;
+
+public class HabitService : IHabitService
+{
+    private readonly GoodHabitsDbContext _dbContext;
+    public HabitService(GoodHabitsDbContext dbContext)
+        => _dbContext = dbContext;
+    public async Task<Habit> Create(string name,
+      string description)
+    {
+        var habit = _dbContext.Habits!.Add(new Habit
+        { Name = name, Description = description }).Entity;
+        await _dbContext.SaveChangesAsync();
+
+        return habit;
+    }
+  public async Task<IReadOnlyList<Habit>> GetAll()
+  {
+    var data = await _dbContext.Habits!.ToListAsync();
+
+    return data;
+  }
+      
+    public async Task<Habit> GetById(int id)
+        => await _dbContext.Habits.FindAsync(id);
+}
